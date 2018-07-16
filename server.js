a52509d362cedebefe7af5d56fef9e2fde3fdd98
Ian Beihl <ianbeihl@gmail.com>
2018-07-16 12:53:49 -0700

    created passport local strategy and protected endpoint

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const foldersRouter = require('./routes/folders');
 const tagsRouter = require('./routes/tags');
 const usersRouter = require('./routes/users');
 
+const passport = require('passport');
+const localStrategy = require('./passport/local');
+const authRouter = require('./routes/auth');
+
+passport.use(localStrategy);
+
 // Create an Express application
 const app = express();
 
@@ -29,7 +35,8 @@ app.use(express.json());
 app.use('/api/notes', notesRouter);
 app.use('/api/folders', foldersRouter);
 app.use('/api/tags', tagsRouter);
-app.use('/api/users', usersRouter)
+app.use('/api/users', usersRouter);
+app.use('/api', authRouter);
 
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
