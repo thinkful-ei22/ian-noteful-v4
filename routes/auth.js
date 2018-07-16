6999722130fe1e4a5d8da84327de6b01860259e8
Ian Beihl <ianbeihl@gmail.com>
2018-07-16 13:38:52 -0700

    added bcrypt and updated endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,12 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
 const passport = require('passport');
-const localStrategy = require('../passport/local');
-
-passport.use(localStrategy);
-
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
-
-
 
 router.post('/login', localAuth, function (req, res) {
   return res.json(req.user);
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const User = require('../models/user');
-
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
 
 //POST ENDPOINT
 
@@ -13,11 +14,32 @@ router.post('/', (req, res, next) => {
     const { fullname, username, password } = req.body;
     const newUser = { fullname, username, password };
 
-    User.create(newUser)
-        .then((result) => {
-            res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    const requiredFields = ['username', 'password'];
+    const missingField = requiredFields.find(field => !(field in req.body));
+  
+    if (missingField) {
+      const err = new Error(`Missing '${missingField}' in request body`);
+      err.status = 422;
+      return next(err);
+    }
+
+    return User.hashPassword(password)
+        .then(digest => {
+            console.log(digest);
+            newUser.password = digest;
+            return User.create(newUser);
         })
-        .catch(err => next(err));
+        .then(result => {
+            return res.status(201).location(`/api/users/${result.id}`).json(result);
+        })
+        .catch(err => {
+            if (err.code === 11000) {
+            err = new Error('The username already exists');
+            err.status = 400;
+            }
+            next(err);
+    });
+    
 });
 
 module.exports = router;
